aca84349bfdd012cb394cc7bc2e600546553161a
NCRobotix <164904835+NCRobotix@users.noreply.github.com>
2024-07-25 00:35:33 -0400

    Update runnerGame.js

--- JS/runnerGame.js
+++ JS/runnerGame.js
@@ -129,28 +129,28 @@ class EndlessRunnerGame {
 }
 
 class button {
-    constructor(text: string, fillColor: string, textColor: string) {
+    constructor(text, fillColor, textColor) {
       this.text = text;
       this.fillColor = fillColor;
       this.textColor = textColor;
     }
-    setPosition(x: number, y: number): void {
+    setPosition(x , y ) {
       this.x = x;
       this.y = y;
     }
-    setSize(width: number, height: number): void {
+    setSize(width , height )  {
       this.width = width;
       this.height = height;
     }
 
-    draw(c: CanvasRenderingContext2D): void {
+   // draw() {
       // draw the button body
-      c.fillStyle = this.fillColor;
-      c.fillRect(this.x, this.y, this.width, this.height);  // draw the button text
-      c.fillStyle = this.textColor;
-      c.textAlign = 'center';
-      c.textBaseline = 'middle';
-      c.font = '25px arial';
-      c.fillText(this.text, this.x + this.width / 2, this.y + this.height / 2, this.width);
-    }
+     // c.fillStyle = this.fillColor;
+     // c.fillRect(this.x, this.y, this.width, this.height);  // draw the button text
+     // c.fillStyle = this.textColor;
+     // c.textAlign = 'center';
+     // c.textBaseline = 'middle';
+      //c.font = '25px arial';
+      //c.fillText(this.text, this.x + this.width / 2, this.y + this.height / 2, this.width);
+    //}
 }
