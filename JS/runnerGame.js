c9cc1566adb1cfb3a3a1753507c4f79f2591fd73
NCRobotix <164904835+NCRobotix@users.noreply.github.com>
2024-07-25 00:13:06 -0400

    Update runnerGame.js

--- JS/runnerGame.js
+++ JS/runnerGame.js
@@ -155,3 +155,30 @@ class EndlessRunnerGame {
         this.ctx.closePath();
     }
 }
+
+class button {
+    constructor(text: string, fillColor: string, textColor: string) {
+      this.text = text;
+      this.fillColor = fillColor;
+      this.textColor = textColor;
+    }
+    setPosition(x: number, y: number): void {
+      this.x = x;
+      this.y = y;
+    }
+    setSize(width: number, height: number): void {
+      this.width = width;
+      this.height = height;
+    }
+
+    draw(c: CanvasRenderingContext2D): void {
+      // draw the button body
+      c.fillStyle = this.fillColor;
+      c.fillRect(this.x, this.y, this.width, this.height);  // draw the button text
+      c.fillStyle = this.textColor;
+      c.textAlign = 'center';
+      c.textBaseline = 'middle';
+      c.font = '25px arial';
+      c.fillText(this.text, this.x + this.width / 2, this.y + this.height / 2, this.width);
+    }
+}
